a486de0e4ba71fffacf177bd2e5d0e9502b59e32
abc <abc@DESKTOP-TK5HSIK>
2022-01-30 18:12:27 +0530

    commit

---
+++ Java_Programs/src/src/Array_2D.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Array_2D {
+
+	public static void main(String[] args)
+	{
+		byte[][] arr=new byte[2][3];
+		System.out.println(arr);
+		System.out.println(arr[0]);
+		System.out.println(arr[0][0]);
+			
+	}
+
+}
