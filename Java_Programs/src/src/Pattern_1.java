714a087ab0e9c0300ac20652df3ed7a5919619a3
abc <abc@DESKTOP-TK5HSIK>
2022-02-02 12:14:45 +0530

    commit

--- Java_Programs/src/src/Pattern_1.java
+++ Java_Programs/src/src/Pattern_1.java
@@ -1,9 +1,24 @@
 package src;
 
+import java.util.Scanner;
+
 public class Pattern_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter the no of lines");
+		int i,j;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for (i=1 ;i<=n ;i++)
+		{
+			for(j=1 ;j<=i ;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println( );
+		}
+		
+		
 
 	}
 
