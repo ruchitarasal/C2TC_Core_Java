8f718f04cc8651f1fe0fd6409b0b399cc2947beb
abc <abc@DESKTOP-TK5HSIK>
2022-02-16 15:02:10 +0530

    commit

---
+++ Java_Programs/src/src/GenericsDemo.java
@@ -0,0 +1,19 @@
+package src;
+
+public class GenericsDemo {
+
+	public static void main(String[] args) {
+	Container<Integer> obj=new Container<>();
+	obj.value=1;
+	
+	obj.show();
+	}
+}
+
+class Container<T>
+{T value;
+public void show()
+
+{
+	System.out.println(value.getClass().getName());
+	}}
