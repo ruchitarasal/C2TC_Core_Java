81a76002164efaf91367cb46ea8e01a5a54f88d1
abc <abc@DESKTOP-TK5HSIK>
2022-03-15 09:25:44 +0530

    Commit

---
+++ TDD/src/TestCaseDemo.java
@@ -0,0 +1,20 @@
+
+import org.junit.jupiter.api.*;
+
+public class TestCaseDemo 
+{
+@RepeatedTest(10)
+	void sum() {
+		int a=10;
+		int b=23;
+		System.out.println(a+b);
+	}
+//@Disabled = To disable from running
+@Test
+void display()
+{
+
+	System.out.println("Executing TDD");
+}
+
+}
